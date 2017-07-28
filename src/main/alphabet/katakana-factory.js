4b369d34f943fad1972d7b3e625f26965742e53c
Rémi Thevenoux <remi.thevenoux@gmail.com>
2017-07-28 11:42:29 +0200

    More complete Katakana

--- src/main/alphabet/katakana-factory.js
+++ src/main/alphabet/katakana-factory.js
@@ -4,7 +4,7 @@ class KatakanaFactory {
     this.singleton = null;
   }
 
-  getInstance(callback) {
+  getInstance2(callback) {
     if (this.singleton) {
       callback(null, this.singleton);
     } else {
@@ -16,6 +16,41 @@ class KatakanaFactory {
             return;
           }
           that.singleton = new KatakanaAlphabet(data);
+          callback(null, that.singleton);
+        });
+    }
+  }
+
+  getInstance(callback) {
+    if (this.singleton) {
+      callback(null, this.singleton);
+    } else {
+      var that = this;
+      JSONLoader.load("alphabet/katakana-data-2.json",
+        (err, data) => {
+          if (err) {
+            callback(err);
+            return;
+          }
+
+          let array = [];
+          for (let conson in data) {
+            let vowels = data[conson];
+            for (let vowel in vowels) {
+              let kana = vowels[vowel];
+              if (kana) {
+                let ipa = conson + vowel;
+                array.push({
+                  kana: kana,
+                  ipa: ipa
+                });
+              }
+            }
+          }
+
+          //array.forEach(x=>console.log(x.kana+" > "+x.ipa));
+
+          that.singleton = new KatakanaAlphabet(array);
           callback(null, that.singleton);
         });
     }

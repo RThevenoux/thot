56288ec4f934cfcfbf1ed23c195fce4df7ab3eab
Rémi Thevenoux <remi.thevenoux@gmail.com>
2018-05-20 11:47:00 +0200

    Use "gulp-inject" for js and css

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,27 @@
 var gulp = require('gulp');
-const del = require('del');
+var del = require('del');
 var runSequence = require('run-sequence');
+var inject = require('gulp-inject');
 
 gulp.task('clean', function () {
     return del(['./dist/**', '!dist'], { force: true });
 });
 
-gulp.task('myJS', function () {
-    return gulp.src("./src/main/**/*.+(js|json)")
+gulp.task('copy-file', function () {
+    return gulp.src("./src/main/**/*.+(js|json|html|css)")
         .pipe(gulp.dest("./dist/"));;
 });
 
-gulp.task('myCSS', function () {
-    return gulp.src("./src/main/**/*.css")
-        .pipe(gulp.dest("./dist/"));;
-});
-
-gulp.task('myHTML', function () {
-    return gulp.src("./src/main/**/*.html")
-        .pipe(gulp.dest("./dist/"));;
+gulp.task('linkage', function () {
+    var target = gulp.src('./dist/index.html');
+    var sources = gulp.src(['./dist/**/*.js', './dist/**/*.css'], { read: false });
+    var options = {"relative":true};
+    return target.pipe(inject(sources,options))
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('build', function (callback) {
-    runSequence('clean', 'myJS', 'myHTML', 'myCSS', function () { });
+    runSequence('clean', 'copy-file', 'linkage', function () { });
 });
 
 

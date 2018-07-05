4c7503051587caf469fd9475caf9c5b44feca21d
Rémi Thevenoux <remi.thevenoux@gmail.com>
2018-07-05 23:13:41 +0200

    rename folder to "ipa-distance"

--- src/main/core/ipa-distance/pct/pct-feature-factory.js
+++ src/main/core/ipa-distance/pct/pct-feature-factory.js
@@ -3,13 +3,13 @@ class PctFeatureFactory {
   }
 
   getFeatureMapper(callback) {
-    return JSONLoader.load("core/distance/pct/pct-feature-mapping.json")
+    return JSONLoader.load("core/ipa-distance/pct/pct-feature-mapping.json")
       .then(data => new PctFeatureMapper(data));
   }
 
   getFeatureSetComparator(callback) {
     var name = "Equal weights on PCT feature";
-    return JSONLoader.load("core/distance/pct/pct-feature-weight.json")
+    return JSONLoader.load("core/ipa-distance/pct/pct-feature-weight.json")
       .then(weights => new FeatureSetComparator(weights, name));
   }
 }
--- src/main/core/ipa-distance/poc/poc-feature-factory.js
+++ src/main/core/ipa-distance/poc/poc-feature-factory.js
@@ -3,13 +3,13 @@ class PocFeatureFactory {
   }
 
   getFeatureMapper(callback) {
-    return JSONLoader.load("core/distance/poc/poc-feature-mapping.json")
+    return JSONLoader.load("core/ipa-distance/poc/poc-feature-mapping.json")
       .then(data => new PocFeatureMapper(data));
   }
 
   getFeatureSetComparator(callback) {
     var name = "Proof of Concept Comparator";
-    return JSONLoader.load("core/distance/poc/poc-feature-weight.json")
+    return JSONLoader.load("core/ipa-distance/poc/poc-feature-weight.json")
       .then(weights => new FeatureSetComparator(weights, name));
   }
 }
